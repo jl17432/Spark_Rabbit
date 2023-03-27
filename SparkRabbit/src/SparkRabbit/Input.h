a39fe47890fa226be3b7ba1c8a35864bd87a3b3b
Zekun Cai <zekun.cai@futurus.co>
2023-03-28 04:12:24 +0800

    Save local changes

--- SparkRabbit/src/SparkRabbit/Input.h
+++ SparkRabbit/src/SparkRabbit/Input.h
@@ -6,12 +6,12 @@ namespace SparkRabbit {
 	class SPARK_API Input
 	{
 	public:
+		//delet this comment
 		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
 		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
 		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
 		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
 		inline static std::pair<float, float> GetMousePos(){ return s_Instance->GetMousePosImpl(); }
-		//static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
 
 	protected:
 		virtual bool IsKeyPressedImpl(int keycode) = 0;
@@ -19,7 +19,6 @@ namespace SparkRabbit {
 		virtual float GetMouseXImpl() = 0;
 		virtual float GetMouseYImpl() = 0;
 		virtual std::pair<float,float> GetMousePosImpl() = 0;
-		//virtual bool IsKeyPressedImpl(int keycode) = 0;
 
 	private:
 		static Input* s_Instance;
